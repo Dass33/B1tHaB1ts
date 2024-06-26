db6f0930c5549abbb4140b17dfddbeb66557559c
Dass33 <danieldvoraku@gmail.com>
2024-06-26 21:15:53 +0200

    Local storage finally works, small css changes, typescript rewrite

--- script.js
+++ script.js
@@ -1,100 +1,128 @@
 var _a, _b;
-var dailiElement = localStorage.getItem('dailiList');
-var itemsArrayDaili = dailiElement ? JSON.parse(dailiElement) : [];
-for (var i = 0; i < itemsArrayDaili.length; i++) {
-    console.log(itemsArrayDaili[i]);
-    addTask(itemsArrayDaili[i], "dailiUL");
-}
-var todosElement = localStorage.getItem('sprintList');
-var itemsArrayTodos = todosElement ? JSON.parse(todosElement) : [];
-var i;
-for (i = 0; i < itemsArrayTodos.length; i++) {
-    addTask(itemsArrayTodos[i], "sprintUL");
-}
-function addTask(text, ulToPaste) {
-    var _a;
-    var li = document.createElement('li');
-    var t = document.createTextNode(text);
-    li.appendChild(t);
-    li.className = "liColumns";
-    (_a = document.getElementById(ulToPaste)) === null || _a === void 0 ? void 0 : _a.appendChild(li);
-}
-var myNodelist = document.getElementsByClassName("liColumns");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("span");
-    var txt = document.createTextNode("X");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-var listItems = Array.from(document.getElementsByClassName("liColumns"));
-listItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-        var _this = this;
-        this.classList.add('checkOffAnim');
-        setTimeout(function() {
-            _this.remove();
-        }, 300);
-    });
-});
-// Click on a close button to hide the current list item
-var closeElements = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < closeElements.length; i++) {
-    closeElements[i].onclick = function() {
-        this.parentElement.remove();
-    };
-}
-(_a = document.getElementById("input-daili")) === null || _a === void 0 ? void 0 : _a.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        newElement("input-daili", "dailiUL", "dailiList");
-    }
-});
-(_b = document.getElementById("input-todos")) === null || _b === void 0 ? void 0 : _b.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        newElement("input-todos", "sprintUL", "sprintList");
-    }
-});
-// Create a new list item when clicking on the "Add" button
 function newElement(inputColumn, columnUL, localStorageVal) {
     var _a;
     var li = document.createElement("li");
     var inputValue = document.getElementById(inputColumn);
     var t = document.createTextNode(inputValue.value);
-    // Adding to local storage
-    var storedData = localStorage.getItem(localStorageVal);
-    var itemsArray = storedData ? JSON.parse(storedData) : [];
-    itemsArray.push(inputValue);
-    localStorage.setItem(localStorageVal, JSON.stringify(itemsArray));
-    //
     li.appendChild(t);
-    if (!inputValue) {
+    if (!inputValue.value) {
         alert("You must write something!");
+        return;
     }
     else {
         li.className = "liColumns";
         (_a = document.getElementById(columnUL)) === null || _a === void 0 ? void 0 : _a.appendChild(li);
     }
-    inputValue.value = "";
     var span = document.createElement("span");
     var txt = document.createTextNode("X");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            this.parentElement.style.display = "none";
-        };
-    }
+    closeOnClick();
     var listItems = document.querySelectorAll('li');
-    listItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            var _this = this;
-            this.classList.add('checkOffAnim');
-            setTimeout(function() {
-                _this.remove();
+    listItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            item.classList.add('checkOffAnim');
+            setTimeout(function () {
+                item.remove();
             }, 300);
         });
     });
+    li.setAttribute('locatedIn', localStorageVal);
+    // Adding to local storage
+    var storedData = localStorage.getItem(localStorageVal);
+    var itemsArray = storedData ? JSON.parse(storedData) : [];
+    itemsArray.push(inputValue.value);
+    localStorage.setItem(localStorageVal, JSON.stringify(itemsArray));
+    //
+    inputValue.value = "";
 }
+// Click on a close button to hide the current list item
+function closeOnClick() {
+    var closeElements = Array.from(document.getElementsByClassName("close"));
+    var _loop_1 = function (element) {
+        element.onclick = function () {
+            var _a;
+            if (element.parentElement) {
+                var locatedIn = element.parentElement.getAttribute('locatedIn');
+                if (locatedIn) {
+                    var storedData = localStorage.getItem(locatedIn);
+                    var itemsArray = storedData ? JSON.parse(storedData) : [];
+                    //implement deleting of the element from the array
+                    var value = (_a = element.parentElement.firstChild) === null || _a === void 0 ? void 0 : _a.nodeValue;
+                    var index = itemsArray.indexOf(value);
+                    itemsArray.splice(index, 1);
+                    localStorage.setItem(locatedIn, JSON.stringify(itemsArray));
+                }
+                element.parentElement.remove();
+            }
+        };
+        var currentBorderColor;
+        var closeColor = getComputedStyle(document.body).getPropertyValue('--closeHover');
+        element.addEventListener('mouseover', function () {
+            if (element.parentElement) {
+                currentBorderColor = element.parentElement.style.borderColor;
+                element.parentElement.style.borderColor = closeColor;
+            }
+        });
+        element.addEventListener('mouseout', function () {
+            if (element.parentElement) {
+                element.parentElement.style.borderColor = currentBorderColor;
+            }
+        });
+    };
+    for (var _i = 0, closeElements_1 = closeElements; _i < closeElements_1.length; _i++) {
+        var element = closeElements_1[_i];
+        _loop_1(element);
+    }
+}
+function addTask(text, ulToPaste, localStorageVal) {
+    var _a;
+    var li = document.createElement('li');
+    var t = document.createTextNode(text);
+    li.appendChild(t);
+    li.className = "liColumns";
+    li.setAttribute('locatedIn', localStorageVal);
+    (_a = document.getElementById(ulToPaste)) === null || _a === void 0 ? void 0 : _a.appendChild(li);
+}
+var dailiElement = localStorage.getItem('dailiList');
+var itemsArrayDaili = dailiElement ? JSON.parse(dailiElement) : [];
+for (var _i = 0, itemsArrayDaili_1 = itemsArrayDaili; _i < itemsArrayDaili_1.length; _i++) {
+    var dailies = itemsArrayDaili_1[_i];
+    addTask(dailies, "dailiUL", 'dailiList');
+}
+var todosElement = localStorage.getItem('sprintList');
+var itemsArrayTodos = todosElement ? JSON.parse(todosElement) : [];
+for (var _c = 0, itemsArrayTodos_1 = itemsArrayTodos; _c < itemsArrayTodos_1.length; _c++) {
+    var todo = itemsArrayTodos_1[_c];
+    addTask(todo, "sprintUL", 'sprintList');
+}
+var myNodelist = Array.from(document.getElementsByClassName("liColumns"));
+for (var _d = 0, myNodelist_1 = myNodelist; _d < myNodelist_1.length; _d++) {
+    var nodes = myNodelist_1[_d];
+    var span = document.createElement("span");
+    var txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    nodes.appendChild(span);
+}
+var listItems = Array.from(document.getElementsByClassName("liColumns"));
+listItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+        item.classList.add('checkOffAnim');
+        setTimeout(function () {
+            item.remove();
+        }, 300);
+    });
+});
+closeOnClick();
+(_a = document.getElementById("input-daili")) === null || _a === void 0 ? void 0 : _a.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        newElement("input-daili", "dailiUL", "dailiList");
+    }
+});
+(_b = document.getElementById("input-todos")) === null || _b === void 0 ? void 0 : _b.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        newElement("input-todos", "sprintUL", "sprintList");
+    }
+});
--- script.ts
+++ script.ts
@@ -1,53 +1,120 @@
+function newElement(inputColumn: string, columnUL: string, localStorageVal: string) {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById(inputColumn) as HTMLInputElement;
+  let t = document.createTextNode(inputValue.value);
+
+  li.appendChild(t);
+  if (!inputValue.value) {
+    alert("You must write something!");
+    return;
+  } else {
+    li.className = "liColumns";
+    document.getElementById(columnUL)?.appendChild(li);
+  }
+
+  let span = document.createElement("span");
+  let txt = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  closeOnClick();
+  let listItems = document.querySelectorAll('li');
+  listItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      item.classList.add('checkOffAnim');
+      setTimeout(() => {
+        item.remove();
+      }, 300);
+    });
+  });
+
+  li.setAttribute('locatedIn', localStorageVal);
+
+  // Adding to local storage
+  let storedData = localStorage.getItem(localStorageVal);
+  let itemsArray = storedData ? JSON.parse(storedData) : [];
+  itemsArray.push(inputValue.value);
+  localStorage.setItem(localStorageVal, JSON.stringify(itemsArray));
+  //
+
+  inputValue.value = "";
+}
+// Click on a close button to hide the current list item
+function closeOnClick() {
+  let closeElements = Array.from(document.getElementsByClassName("close"));
+  for (let element of closeElements) {
+    (element as HTMLElement).onclick = function() {
+      if (element.parentElement) {
+        let locatedIn = element.parentElement.getAttribute('locatedIn');
+        if (locatedIn) {
+          let storedData = localStorage.getItem(locatedIn);
+          let itemsArray = storedData ? JSON.parse(storedData) : [];
+          //implement deleting of the element from the array
+          let value = element.parentElement.firstChild?.nodeValue;
+          let index = itemsArray.indexOf(value);
+          itemsArray.splice(index, 1);
+          localStorage.setItem(locatedIn, JSON.stringify(itemsArray));
+        }
+        element.parentElement.remove();
+      }
+    }
+    let currentBorderColor: string;
+    let closeColor = getComputedStyle(document.body).getPropertyValue('--closeHover');
+    element.addEventListener('mouseover', function() {
+      if (element.parentElement) {
+        currentBorderColor = element.parentElement.style.borderColor;
+        element.parentElement.style.borderColor = closeColor;
+      }
+    });
+    element.addEventListener('mouseout', function() {
+      if (element.parentElement) {
+        element.parentElement.style.borderColor = currentBorderColor;
+      }
+    });
+  }
+}
+
+function addTask(text: string, ulToPaste: string, localStorageVal: string) {
+  const li = document.createElement('li');
+  let t = document.createTextNode(text);
+  li.appendChild(t);
+  li.className = "liColumns";
+  li.setAttribute('locatedIn', localStorageVal);
+  document.getElementById(ulToPaste)?.appendChild(li);
+}
+
 let dailiElement = localStorage.getItem('dailiList');
 let itemsArrayDaili = dailiElement ? JSON.parse(dailiElement) : [];
-for (var i = 0; i < itemsArrayDaili.length; i++) {
-  console.log(itemsArrayDaili[i]);
-  addTask(itemsArrayDaili[i], "dailiUL");
+for (let dailies of itemsArrayDaili) {
+  addTask(dailies, "dailiUL", 'dailiList');
 }
 
 let todosElement = localStorage.getItem('sprintList');
 let itemsArrayTodos = todosElement ? JSON.parse(todosElement) : [];
-var i: number;
-for (i = 0; i < itemsArrayTodos.length; i++) {
-  addTask(itemsArrayTodos[i], "sprintUL");
+for (let todo of itemsArrayTodos) {
+  addTask(todo, "sprintUL", 'sprintList');
 }
 
-function addTask(text: string, ulToPaste: string) {
-  const li = document.createElement('li');
-  var t = document.createTextNode(text);
-  li.appendChild(t);
-  li.className = "liColumns";
-  document.getElementById(ulToPaste)?.appendChild(li);
-}
-
-var myNodelist = document.getElementsByClassName("liColumns");
-var i: number;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("X");
+var myNodelist = Array.from(document.getElementsByClassName("liColumns"));
+for (let nodes of myNodelist) {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("X");
   span.className = "close";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+  nodes.appendChild(span);
 }
 
 const listItems = Array.from(document.getElementsByClassName("liColumns"));
 listItems.forEach(function(item) {
   item.addEventListener('click', function() {
-    this.classList.add('checkOffAnim');
+    item.classList.add('checkOffAnim');
     setTimeout(() => {
-      this.remove();
+      item.remove();
     }, 300);
   });
 });
 
-// Click on a close button to hide the current list item
-var closeElements = document.getElementsByClassName("close");
-var i: number;
-for (i = 0; i < closeElements.length; i++) {
-  closeElements[i].onclick = function() {
-    this.parentElement.remove();
-  }
-}
+closeOnClick();
 
 document.getElementById("input-daili")?.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
@@ -60,47 +127,3 @@ document.getElementById("input-todos")?.addEventListener('keydown', function(eve
     newElement("input-todos", "sprintUL", "sprintList");
   }
 });
-
-// Create a new list item when clicking on the "Add" button
-function newElement(inputColumn: string, columnUL: string, localStorageVal: string) {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById(inputColumn) as HTMLInputElement;
-  var t = document.createTextNode(inputValue.value);
-
-  // Adding to local storage
-  let storedData = localStorage.getItem(localStorageVal);
-  var itemsArray = storedData ? JSON.parse(storedData) : [];
-  itemsArray.push(inputValue);
-  localStorage.setItem(localStorageVal, JSON.stringify(itemsArray));
-  //
-
-  li.appendChild(t);
-  if (!inputValue) {
-    alert("You must write something!");
-  } else {
-    li.className = "liColumns";
-    document.getElementById(columnUL)?.appendChild(li);
-  }
-  inputValue.value = "";
-
-  var span = document.createElement("span");
-  var txt = document.createTextNode("X");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      this.parentElement.style.display = "none";
-    }
-  }
-  var listItems = document.querySelectorAll('li');
-  listItems.forEach(function(item) {
-    item.addEventListener('click', function() {
-      this.classList.add('checkOffAnim');
-      setTimeout(() => {
-        this.remove();
-      }, 300);
-    });
-  });
-}
